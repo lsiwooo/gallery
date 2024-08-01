135cfdbe1564872e51c40f990cca7e5a379b662f
lsiwooo <iwsl3205@gmail.com>
2024-08-01 17:07:05 +0900

    upload files

--- app/src/main/java/com/example/moe/MainActivity.kt
+++ app/src/main/java/com/example/moe/MainActivity.kt
@@ -1,20 +1,124 @@
 package com.example.moe
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.provider.MediaStore
+import android.provider.Settings
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.moe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val TAG = "MainActivity"
+    private val REQUEST_READ_EXTERNAL_STORAGE = 100
+    private val REQUEST_PICK_IMAGE = 101
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+    }
+
+
+    private fun checkGalleryPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+            ) {
+                alert("사진 정보를 얻으려면 외부 저장소 권한이 필수로 필요합니다", "권한이 필요한 이유") {
+                    yesButton {
+                        ActivityCompat.requestPermissions(
+                            this@MainActivity,
+                            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                            REQUEST_READ_EXTERNAL_STORAGE
+                        )
+                    }
+                    noButton {}
+                }.show()
+            } else {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    REQUEST_READ_EXTERNAL_STORAGE
+                )
+            }
+        } else {
+            getALLPhotos()
         }
     }
+
+    private fun yesButton(function: () -> Unit) {
+        TODO("Not yet implemented")
+    }
+
+    private fun noButton(function: () -> Unit) {
+        TODO("Not yet implemented")
+    }
+
+    private fun alert(s: String, s1: String, function: () -> Unit): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_READ_EXTERNAL_STORAGE -> {
+                if ((grantResults.isNotEmpty()
+                            && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                ) {
+                    getALLPhotos()
+                } else {
+                    toast("권한 거부 됨")
+                }
+                return
+            }
+        }
+    }
+
+    private fun toast(s: String) {
+
+    }
+
+    private fun getALLPhotos() {
+        val cursor = contentResolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            null,
+            null,
+            null,
+            MediaStore.Images.ImageColumns.DATE_TAKEN + "DESC"
+        )
+
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                val uri =
+                    cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA))
+                Log.d("MainActivity", uri)
+            }
+            cursor.close()
+        }
+    }
+}
+
+private fun Any.show() {
+    TODO("Not yet implemented")
 }
